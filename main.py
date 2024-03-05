e49c00f61220e4990661fd058135e06361145148
SlanSaf <slansaf@yandex.ru>
2024-03-05 20:03:39 +0300

    add file main

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import asyncio
+import config
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+import logging
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#подключение логгирование
+logging.basicConfig(level=logging.INFO)
 
+# создаем объект бота
+bot = Bot(token=config.token)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Диспечер
+dispecher = Dispatcher()
 
+#Хендлер на команду /start
+@dispecher.message(Command('start'))
+async def cmd_start(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Привет, {name}')
 
-# Press the green button in the gutter to run the script.
+#Хендлер на команду /stop
+@dispecher.message(Command('stop'))
+async def cmd_stop(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Пока, {name}')
+
+#Хендлер на собщения
+@dispecher.message(F.text)
+async def msg_echo(message: types.Message):
+    msg_user = message.text
+    name = message.chat.first_name
+    if 'привет' in msg_user:
+        await message.answer(f'Привет - {name}')
+    elif 'пока' in msg_user:
+        await message.answer(f'Пока - {name}')
+    elif 'ты кто' in msg_user:
+        await message.answer(f'Я бот - {name}')
+    else:
+        await message.answer(f'Я не знаю такого слова!')
+    await  message.answer(f'Ты написал - {msg_user}')
+
+async def main():
+    await dispecher.start_polling(bot)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
