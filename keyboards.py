5f14f8a9153907b545957dcdf039330576f199dc
SlanSaf <slansaf@yandex.ru>
2024-03-11 19:54:04 +0300

    Add buttons in chat bot

---
+++ keyboards.py
@@ -0,0 +1,19 @@
+from aiogram import types
+
+
+button1 = types.KeyboardButton(text='/start')
+button2 = types.KeyboardButton(text='Стоп')
+button3 = types.KeyboardButton(text='Инфо')
+button4 = types.KeyboardButton(text='Покажи лису')
+button5 = types.KeyboardButton(text='Закрыть')
+keyboard1 = [
+    [button1, button2, button3],
+]
+
+keyboard2 = [
+    [button4, button5],
+]
+
+kb1 = types.ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
+
+kb2 = types.ReplyKeyboardMarkup(keyboard=keyboard2, resize_keyboard=True)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 import logging
 import random
+from keyboards import kb1, kb2
+from random_fox import fox
 
 #подключение логгирование
 logging.basicConfig(level=logging.INFO)
@@ -18,13 +20,24 @@ dispecher = Dispatcher()
 @dispecher.message(Command('start'))
 async def cmd_start(message: types.Message):
     name = message.chat.first_name
-    await message.answer(f'Привет, {name}')
+    await message.answer(f'Привет, {name}', reply_markup=kb1)
 
 #Хендлер на команду /stop
 @dispecher.message(Command('stop'))
 async def cmd_stop(message: types.Message):
     name = message.chat.first_name
     await message.answer(f'Пока, {name}')
+
+#Хендлейр на команду  /fox
+@dispecher.message(Command('fox'))
+@dispecher.message(Command('лиса'))
+@dispecher.message(F.text.lower() == 'покажи лису')
+async def cmd_fox(message: types.Message):
+    name = message.chat.first_name
+    img_fox = fox()
+    await message.answer(f'Держи лису, {name}')
+    await message.answer_photo(photo=img_fox)
+    #await bot.send_photo(message.from_user.id, photo=img_fox)
 
 #Хендлер на собщения
 @dispecher.message(F.text)
@@ -36,10 +49,12 @@ async def msg_echo(message: types.Message):
     elif 'пока' in msg_user:
         await message.answer(f'Пока - {name}')
     elif 'ты кто' in msg_user:
-        await message.answer(f'Я бот - {name}')
+        await message.answer_dice(emoji="")
+    elif 'лису' in msg_user:
+        await message.answer(f'Смотри у меня есть, {name}', reply_markup=kb2)
     else:
         await message.answer(f'Я не знаю такого слова!')
-    await  message.answer(f'Ты написал - {msg_user}')
+    await message.answer(f'Ты написал - {msg_user}')
 
 async def main():
     await dispecher.start_polling(bot)
